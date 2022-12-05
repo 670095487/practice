cb1c1b5eee1f5d96b90016fe69bafeda286fce36
yunN <670095487@qq.com>
2022-12-05 14:01:22 +0800

    delete useless module.

---
+++ autumn/src/main/java/com/yunn/autumn/leetcode/LC4.java
@@ -0,0 +1,17 @@
+package com.yunn.autumn.leetcode;
+
+/**
+ * @author yunN
+ * @date 2022/12/05.
+ */
+public class LC4 {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double ans = 0;
+
+
+
+
+        return ans;
+    }
+}
